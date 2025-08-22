099943c15208d32f7a22da7d18ac9ddf762f83c8
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2025-08-22 15:35:07 -0300

    Category find by id

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -20,5 +21,10 @@ public class CategoryService {
 	public List<CategoryDTO> findAll() {
 		List<Category> result = repository.findAll();
 		return result.stream().map(x -> new CategoryDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		return new CategoryDTO(repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not found")));
 	}
 }
