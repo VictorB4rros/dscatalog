60ad3a042ad86b9fdea63e5dc139e18b4b76e442
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2025-09-30 00:28:06 -0300

    User constructor correction

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -44,9 +44,10 @@ public class User implements UserDetails {
 	public User() {
 	}
 
-	public User(Long id, String firstName, String email, String password) {
+	public User(Long id, String firstName, String lastName, String email, String password) {
 		this.id = id;
 		this.firstName = firstName;
+		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
 	}
