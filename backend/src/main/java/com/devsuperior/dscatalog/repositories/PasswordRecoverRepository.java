4473082a755de59c18a13e1d25a668cd596c4a31
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2026-05-05 00:02:39 -0300

    PasswordRecover entity and repository added, password recovery email use case added

---
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/PasswordRecoverRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.PasswordRecover;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PasswordRecoverRepository extends JpaRepository<PasswordRecover, Long> {
+
+}
